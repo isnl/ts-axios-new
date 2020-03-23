a9e73ea4c8c66b733b7ca2d6cbaa3e4a9e7822e9
candy <htmlcs@163.com>
2020-03-23 13:57:31 +0800

    处理data,处理headers

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,7 +1,22 @@
 const toString = Object.prototype.toString
+/**
+ * 是否为时间
+ * @param val
+ */
 export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
+/**
+ * 是否为对象
+ * @param val
+ */
 export function isObject(val: any): val is Object {
   return val !== null && typeof val === 'object'
+}
+/**
+ * 是否为普通对象
+ * @param val
+ */
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from './types'
 import { buildURL } from './helper/url'
+import { transformRequest } from './helper/data'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -7,17 +8,23 @@ function axios(config: AxiosRequestConfig): void {
 }
 /**
  * 处理请求config
- * @param config 
+ * @param config
  */
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 /**
  * 处理url
- * @param config 
+ * @param config
  */
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  const { data } = config
+  return transformRequest(data)
 }
 export default axios
